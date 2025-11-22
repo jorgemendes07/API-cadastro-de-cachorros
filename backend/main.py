12d742eb43f6038283d4a5cec9d0543e26a8167e
Jorge Mendes <jorge.mendes7@hotmail.com>
2025-11-22 17:05:27 -0300

    Cria a classe Cachorro no main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,13 @@
+from datetime import date
+from sqlmodel import Field, SQLModel
 from fastapi import FastAPI
 
+class Cachorro(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    nome: str = Field(index=True, max_length=100)
+    raca: str = Field(max_length=50)
+    data_nascimento: date
+    porte: str = Field(max_length=20)
 
 app = FastAPI()
 
