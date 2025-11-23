7908bf44590a3020fdfe747ab71f099f918939ed
Jorge Mendes <jorge.mendes7@hotmail.com>
2025-11-22 21:12:11 -0300

    Configura o banco de dados sqlite e cria a tabela cachorros.db

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from datetime import date
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, create_engine
 from fastapi import FastAPI
+
+# Model
 
 class Cachorro(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -9,7 +11,20 @@ class Cachorro(SQLModel, table=True):
     data_nascimento: date
     porte: str = Field(max_length=20)
 
+# Configuração do banco de dados
+
+sqlite_file_name = "cachorros.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url)
+
+# Cria a tabela
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
 app = FastAPI()
+
+create_db_and_tables()
 
 @app.get('/')
 def hello():
