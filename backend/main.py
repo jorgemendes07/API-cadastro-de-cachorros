74d61aa60fa5c526e299e9b937c0a41bf7c6491a
Jorge Mendes <jorge.mendes7@hotmail.com>
2025-11-23 17:44:08 -0300

    Adiciona a rota para criacao de Cachorro e muda o tipo de date para string para corrigir o erro 500

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from datetime import date
 from sqlmodel import Field, SQLModel, create_engine, Session
+from sqlalchemy import String
 from fastapi import FastAPI, Depends
 
 # Model
@@ -8,7 +9,7 @@ class Cachorro(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     nome: str = Field(index=True, max_length=100)
     raca: str = Field(max_length=50)
-    data_nascimento: date
+    data_nascimento: date = Field(sa_type=String)
     porte: str = Field(max_length=20)
 
 # Configuração do db
@@ -34,6 +35,9 @@ app = FastAPI()
 
 create_db_and_tables()
 
-@app.get('/')
-def hello():
-    return {"message": "Hello!"}
+@app.post('/cachorros')
+def cadastrar_cachorro(cachorro: Cachorro, session: Session = Depends(get_session)):
+    session.add(cachorro)
+    session.commit()
+    session.refresh(cachorro)
+    return cachorro
