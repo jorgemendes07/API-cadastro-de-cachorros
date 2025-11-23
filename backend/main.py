d639642dc1b5984f15db3aa341aaafab54840882
Jorge Mendes <jorge.mendes7@hotmail.com>
2025-11-23 00:10:55 -0300

    Adiciona a dependencia do banco de dados no main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from datetime import date
-from sqlmodel import Field, SQLModel, create_engine
-from fastapi import FastAPI
+from sqlmodel import Field, SQLModel, create_engine, Session
+from fastapi import FastAPI, Depends
 
 # Model
 
@@ -11,16 +11,24 @@ class Cachorro(SQLModel, table=True):
     data_nascimento: date
     porte: str = Field(max_length=20)
 
-# Configuração do banco de dados
+# Configuração do db
 
 sqlite_file_name = "cachorros.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-engine = create_engine(sqlite_url)
+engine = create_engine(sqlite_url, connect_args={"check_same_thread": False})
 
 # Cria a tabela
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
+# Dependência do db
+def get_session():
+    session = Session(engine)
+    try:
+        yield session
+    finally:
+        session.close()
 
 app = FastAPI()
 
