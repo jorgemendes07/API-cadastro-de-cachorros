fe133b99e278ffe7ba7ebbfaaa620af8045d0ec4
Jorge Mendes <jorge.mendes7@hotmail.com>
2025-11-23 18:28:52 -0300

    Adiciona rota de visualizacao de todos os cachorros e adiciona rota de visualizacao individual por id

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from datetime import date
-from sqlmodel import Field, SQLModel, create_engine, Session
+from sqlmodel import Field, SQLModel, create_engine, Session, select
 from sqlalchemy import String
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 
 # Model
 
@@ -35,9 +35,26 @@ app = FastAPI()
 
 create_db_and_tables()
 
-@app.post('/cachorros')
+# Criar cachorro
+@app.post("/cachorros")
 def cadastrar_cachorro(cachorro: Cachorro, session: Session = Depends(get_session)):
     session.add(cachorro)
     session.commit()
     session.refresh(cachorro)
     return cachorro
+
+# Listar todos os cachorros
+@app.get("/cachorros", response_model=list[Cachorro])
+def listar_cachorros(session: Session = Depends(get_session)):
+    selecao = select(Cachorro)
+    resultado = session.exec(selecao)
+    cachorros = resultado.all()
+    return cachorros
+
+# Visualizar cachorro pela id
+@app.get("/cachorros/{cachorro_id}", response_model=Cachorro)
+def selecionar_cachorro_pelo_id(cachorro_id: int, session: Session = Depends(get_session)):
+    cachorro_selecionado = session.get(Cachorro, cachorro_id)
+    if not cachorro_selecionado:
+        raise HTTPException(status_code=404, detail="Cachorro não encontrado")
+    return cachorro_selecionado
