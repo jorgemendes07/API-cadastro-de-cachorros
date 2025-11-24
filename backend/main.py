071a750cbffacfbe56f56c6dfd57cfd4f2fdbf1b
Jorge Mendes <jorge.mendes7@hotmail.com>
2025-11-24 16:24:03 -0300

    Adiciona rota de atualizacao de registro de Cachorro

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,6 @@ from sqlalchemy import String
 from fastapi import FastAPI, Depends, HTTPException
 
 # Model
-
 class Cachorro(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     nome: str = Field(index=True, max_length=100)
@@ -13,7 +12,6 @@ class Cachorro(SQLModel, table=True):
     porte: str = Field(max_length=20)
 
 # Configuração do db
-
 sqlite_file_name = "cachorros.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
@@ -35,7 +33,7 @@ app = FastAPI()
 
 create_db_and_tables()
 
-# Criar cachorro
+# Criar Cachorro
 @app.post("/cachorros")
 def cadastrar_cachorro(cachorro: Cachorro, session: Session = Depends(get_session)):
     session.add(cachorro)
@@ -51,10 +49,28 @@ def listar_cachorros(session: Session = Depends(get_session)):
     cachorros = resultado.all()
     return cachorros
 
-# Visualizar cachorro pela id
+# Visualizar Cachorro pela id
 @app.get("/cachorros/{cachorro_id}", response_model=Cachorro)
 def selecionar_cachorro_pelo_id(cachorro_id: int, session: Session = Depends(get_session)):
     cachorro_selecionado = session.get(Cachorro, cachorro_id)
     if not cachorro_selecionado:
         raise HTTPException(status_code=404, detail="Cachorro não encontrado")
+    return cachorro_selecionado
+
+# Atualizar Cachorro
+@app.put("/cachorros/{id}", response_model=Cachorro)
+def atualizar_cachorro(cachorro_id: int, novos_dados: Cachorro, session: Session = Depends(get_session)):
+    cachorro_selecionado = session.get(Cachorro, cachorro_id)
+    if not cachorro_selecionado:
+        raise HTTPException(status_code=404, detail="Cachorro não encontrado")
+    
+    dados_dict = novos_dados.model_dump(exclude_unset=True)
+    dados_dict.pop("id", None)
+
+    for key, value in dados_dict.items():
+        setattr(cachorro_selecionado, key, value)
+
+    session.add(cachorro_selecionado)
+    session.commit()
+    session.refresh(cachorro_selecionado)
     return cachorro_selecionado
