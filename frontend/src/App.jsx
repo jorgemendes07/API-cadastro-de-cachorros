69af72fd1cc9e450dcc79441cd7994054237115d
Jorge Mendes <jorge.mendes7@hotmail.com>
2025-11-26 23:15:09 -0300

    Cria o componente CreateButton e o adiciona no App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import DogTable from './components/DogTable'
+import CreateButton from './components/CreateButton'
 function App() {
   
   return (
     <>
+      <CreateButton />
       <DogTable />
     </>
   )
---
+++ frontend/src/components/CreateButton/index.jsx
@@ -0,0 +1,8 @@
+export default function CreateButton() {
+    return (
+        <button 
+            className="ml-[5%] mt-5 bg-white p-3 cursor-pointer hover:bg-gray-300 ">
+                Adicionar Cachorro
+        </button>
+    )
+}
