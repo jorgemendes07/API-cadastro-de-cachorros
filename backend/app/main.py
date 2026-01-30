be16113f451ba9b4545d0f9850374ab2d1264fd1
Jorge Mendes <jorge.mendes7@hotmail.com>
2026-01-30 19:44:25 -0300

    adiciona checkfirst para verificar se a tabela não existe antes de tentar criar

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ from app.seed import seed_database
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(engine, checkfirst=True)
     seed_database()
     print("Banco criado e pupulado")
     yield
